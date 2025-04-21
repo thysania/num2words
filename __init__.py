e64d2ea9670e4cd702509e2bd377e0c031f88523
thysania <133405532+thysania@users.noreply.github.com>
2025-04-21 16:14:11 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
+# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
+
+from __future__ import unicode_literals
+
+from . import (lang_MA, lang_AR, lang_EN, lang_ES, lang_FR,)
+
+CONVERTER_CLASSES = {
+    'ma': lang_MA.Num2Word_MA(),
+    'ar': lang_AR.Num2Word_AR(),
+    'en': lang_EN.Num2Word_EN(),
+    'fr': lang_FR.Num2Word_FR(),
+    'es': lang_ES.Num2Word_ES(),
+
+}
+
+CONVERTES_TYPES = ['cardinal', 'ordinal', 'ordinal_num', 'year', 'currency']
+
+
+def num2words(number, ordinal=False, lang='en', to='cardinal', **kwargs):
+    # We try the full language first
+    if lang not in CONVERTER_CLASSES:
+        # ... and then try only the first 2 letters
+        lang = lang[:2]
+    if lang not in CONVERTER_CLASSES:
+        raise NotImplementedError()
+    converter = CONVERTER_CLASSES[lang]
+
+    if isinstance(number, str):
+        number = converter.str_to_number(number)
+
+    # backwards compatible
+    if ordinal:
+        to = 'ordinal'
+
+    if to not in CONVERTES_TYPES:
+        raise NotImplementedError()
+
+    return getattr(converter, 'to_{}'.format(to))(number, **kwargs)
